64a0d897bee575f5548877decf2eae708c3e1095
cr-marcstevens <github@marc-stevens.nl>
2024-03-21 09:39:06 +0100

    last state of textcoll

--- src/md5helper/textcoll.cpp
+++ src/md5helper/textcoll.cpp
@@ -323,6 +323,16 @@ struct message_alphabet {
 				word_size[w] *= bytes[w*4+b].size();
 		}
 	}
+	
+	void set_byte_alphabet(int b, const std::string& alphabet)
+	{
+		bytes[b] = byte_alphabet(alphabet);
+		// update corresponding word_size
+		unsigned w = b/4;
+		word_size[w] = 1;
+		for (unsigned i = 0; i < 4; ++i)
+			word_size[w] *= bytes[w*4+i].size();
+	}
 
 	
 	bool checkbyte(int b, uint8_t v) const
@@ -586,9 +596,10 @@ struct textcoll_solver_t {
 	}
 	void findcollision(const vector<differentialpath>& diffpaths, const std::string& alphabet)
 	{
+		fillalphabet(alphabet);
+
 		diffpath = diffpaths.front();
 		filltables();
-		fillalphabet(alphabet);
 
 		if (ihv2[0] == ihv1[0])
 		{
@@ -713,6 +724,32 @@ void textcoll_solver_t::start()
 		vector< halfstate_t > in;
 		vector< halfstate_t >& out = Q7Q24states;
 
+/*
+		MA.set_byte_alphabet(0, "B");
+		MA.set_byte_alphabet(1, "A");
+		MA.set_byte_alphabet(2, "S");
+		MA.set_byte_alphabet(3, "E");
+		MA.set_byte_alphabet(4, "X");
+		MA.set_byte_alphabet(5, "6");
+		MA.set_byte_alphabet(6, "4");
+		MA.set_byte_alphabet(7, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/");
+/*
+//		MA.set_byte_alphabet(16, "0123456789");
+//		MA.set_byte_alphabet(17, "0123456789");
+//		MA.set_byte_alphabet(18, "0123456789");
+//		MA.set_byte_alphabet(19, "0123456789");
+*/
+/*		MA.set_byte_alphabet(20, "bB");
+		MA.set_byte_alphabet(21, "eEiI"); // diff: +4
+		MA.set_byte_alphabet(22, "eE");
+		MA.set_byte_alphabet(23, "rR");
+
+		MA.set_byte_alphabet(60, "wW");
+		MA.set_byte_alphabet(61, "hH");
+		MA.set_byte_alphabet(62, "aA");
+		MA.set_byte_alphabet(63, "tT");
+*/
+
 		// Q17-Q13,m1
 		generateQ13Q17(out, N);
 
@@ -761,6 +798,29 @@ void textcoll_solver_t::start()
 		
 		std::cout << "Saving 'Q7Q24.bin.gz'..." << std::flush;
 		save_gz(out, "Q7Q24", binary_archive);
+		
+		std::map<char, size_t> charcount;
+		for (int wt : { 0, 1, 4, 5, 6, 10, 11, 12, 13, 14, 15 })
+		{
+			for (unsigned b = 0; b < 4; ++b)
+			{
+				charcount.clear();
+				for (auto& S: out)
+					++charcount[ char((S.m[wt]>>(8*b))&0xFF) ];
+				std::cout << "m" << wt << "[" << b << "]=msg[" << (wt*4+b) << "]: ";
+				std::vector< std::pair<char,size_t> > charcountsorted(charcount.begin(), charcount.end());
+				std::sort(charcountsorted.begin(), charcountsorted.end(), [](auto& l, auto& r){ return l.second > r.second; });
+				for (auto& cc : charcountsorted)
+					std::cout << cc.first;
+				std::cout << std::endl;
+				std::cout << "m" << wt << "[" << b << "]=msg[" << (wt*4+b) << "]: ";
+				std::cout.precision(4);
+				for (auto& cc : charcountsorted)
+					std::cout << " " << cc.first << ":" << double(cc.second)/double(out.size());
+				std::cout << std::endl << std::endl;
+			}
+		}
+		exit(0);
 	}
 
 	// sort on maximum Q9m9 tunnel	
@@ -1101,9 +1161,15 @@ void textcoll_solver_t::completeQ7Q24(const halfstate_t& Q7Q24state)
 		S.m[4] = m4;
 		compute_good_m10(S);
 		std::cout << "m4=" << word2str(m4) << ": good_m10 size: " << good_m10.size() << std::endl;
-		if (good_m10.size() < (1ULL<<14))
+		if (good_m10.size() < 6000) //(1ULL<<14))
 			continue;
 		++m4ok;
+		
+		std::cout << "MSG: ";
+		for (unsigned t = 0; t < 16; ++t)
+			for (unsigned b = 0; b < 4; ++b)
+				std::cout << char( (S.m[t]>>(b*8))&0xFF);
+		std::cout << std::endl;
 
 		uint32_t m13org = S.m[13], Q10org = S.Qt(10);
 
@@ -1205,7 +1271,7 @@ void textcoll_solver_t::completeQ7Q24(const halfstate_t& Q7Q24state)
 		
 
 		// iterate over those m12, and find all (m10, m12) satisfying Q7-Q9, Q22-Q23
-		static const size_t maxLUT = 1ULL<<34;
+		static const size_t maxLUT = 1ULL<<30;
 		
 		progress_display pdm12m13(Q7810m1213.size());
 
@@ -1586,6 +1652,7 @@ void textcoll_solver_t::check_solution(const fullstate_t& sol)
         save_block(of2, block2);
         of1.close();
         of2.close();
+        exit(0);
 }
 
 void textcoll_solver_t::compute_good_m10(const fullstate_t& Sin)
@@ -1664,12 +1731,74 @@ void textcoll_solver_t::filltables()
 		dR[offset+t] = dQ[offset+t+1] - dQ[offset+t];
 	}
 
+#if 0
+	counter_exponential_print attempt("attempt"), ok("ok"), partial("partial");
+	fullstate_t S1, S2;
+	while (true)
+	{
+		++attempt;
+		S1.Qt(21) = masked_value_Qt(21,S1).sample();
+		S1.Qt(22) = masked_value_QtQtm1(22,S1).sample();
+		S1.Qt(23) = masked_value_QtQtm1(23,S1).sample();
+		S1.Qt(24) = masked_value_QtQtm1(24,S1).sample();
+		for (int k = 0; k < 16; ++k)
+		{
+			S1.m[k] = xrng64();
+			S2.m[k] = S1.m[k] + m_diff[k];
+		}
+		for (int t = 21; t <= 24; ++t)
+			S2.Qt(t) = S1.Qt(t) + dQt(t);
+		for (int t = 24; t < 64; ++t)
+		{
+			S1.computeQtp1(t);
+			S2.computeQtp1(t);
+		}
+		if (S2.Qt(61)-S1.Qt(61) != uint32_t(1)<<31)
+			continue;
+		++partial;
+		if (S2.Qt(62)-S1.Qt(62) != uint32_t(1)<<31)
+			continue;
+		if (S2.Qt(63)-S1.Qt(63) != uint32_t(1)<<31)
+			continue;
+		if (S2.Qt(64)-S1.Qt(64) != uint32_t(1)<<31)
+			continue;
+		if ( (S1.Qt(64)>>31) != (S1.Qt(63)>>31) )
+			continue;
+		if ( (S1.Qt(64)>>31) != (S1.Qt(62)>>31) )
+			continue;
+		++ok;
+		std::cout << "p=" << log(double(ok())/double(attempt()))/log(2.0) << " partial";
+		std::cout << "p=" << log(double(ok())/double(partial()))/log(2.0) << std::endl;
+	}
+#endif
 //	Q[0] = Qvalue[0]; Q2[0] = Q[0] + dQ[0];
 //	Q[1] = Qvalue[1]; Q2[1] = Q[1] + dQ[1];
 //	Q[2] = Qvalue[2]; Q2[2] = Q[2] + dQ[2];
 //	Q[3] = Qvalue[3]; Q2[3] = Q[3] + dQ[3];
 }
 
+
+bool test_collision(const uint32_t ihv1[4], const uint32_t ihv2[4], const uint32_t m[16])
+{
+	uint32_t tihv1[4];
+	uint32_t tihv2[4];
+	for (unsigned k = 0; k < 4; ++k)
+	{
+		tihv1[k] = ihv1[k];
+		tihv2[k] = ihv2[k];
+	}
+	md5compress(tihv1, m);
+	md5compress(tihv2, m);
+	if (tihv1[0] == tihv2[0])
+		std::cout << "partial solution found" << std::endl;
+	bool collision = true;
+	for (unsigned k = 0; k < 4; ++k)
+		if (tihv1[k] != tihv2[k])
+			collision = false;
+	return collision;
+	
+
+}
 
 void textcoll_solver_t::start_block2()
 {
@@ -1684,67 +1813,249 @@ void textcoll_solver_t::start_block2()
 	if ( (S1.Qt(0)>>31) != (S1.Qt(-1)>>31) || (S1.Qt(-1)>>31) != (S1.Qt(-2)>>31) )
 		throw std::runtime_error("IHV1 and IHV2 do not fulfill the necessary conditions such that dF0,dF1 = 1<<31.");
 
-run_workload(threads,
-   [this]
-   (size_t ji, size_t jn)
-   {
-	fullstate_t S1;
-	S1.Qt(-3) = ihv1[0]; S1.Qt(-2) = ihv1[3]; S1.Qt(-1) = ihv1[2]; S1.Qt(0) = ihv1[1];
-	while (true)
+#if 0
+	// do padding and strengthening in second block		
+	run_workload(threads,[this](size_t ji, size_t jn)
 	{
-		for (int t = 0; t < 15; ++t)
+		fullstate_t S1;
+		S1.Qt(-3) = ihv1[0]; S1.Qt(-2) = ihv1[3]; S1.Qt(-1) = ihv1[2]; S1.Qt(0) = ihv1[1];
+		
+		uint64_t totalbits = (64+8) * 8;
+		S1.m[2] = 0x80;
+		for (unsigned int t = 3; t < 14; ++t)
+			S1.m[t] = 0;
+		S1.m[14] = uint32_t(totalbits & 0xFFFFFFFF);
+		S1.m[15] = uint32_t(totalbits >> 32);
+		
+		size_t cntr = 0;
+		for (uint32_t m0 : MA.word_range(0))
 		{
-			while (true)
+			++cntr; if (cntr > jn) cntr -= jn;
+			if (cntr != ji)
+				continue;
+			
+			S1.m[0] = m0;
+			S1.computeQtp1(0);
+			if ( (S1.Qt(1)>>31) != (S1.Qt(0)>>31) )
+				continue;
+			
+			for (uint32_t m1 : MA.word_range(1))
 			{
-				S1.m[t] = MA.sampleword(t);
-				S1.computeQtp1(t);
-				if ( (S1.Qt(t+1)>>31) == (S1.Qt(t)>>31) )
-					break;
-			}
-		}
-		for (uint32_t m15 : MA.word_range(15))
-		{
-			S1.m[15] = m15;
-			uint32_t tihv1[4];
-			uint32_t tihv2[4];
-			for (unsigned k = 0; k < 4; ++k)
-			{
-				tihv1[k] = ihv1[k];
-				tihv2[k] = ihv2[k];
-			}
-			md5compress(tihv1, S1.m);
-			md5compress(tihv2, S1.m);
-			bool collision = true;
-			for (unsigned k = 0; k < 4; ++k)
-				if (tihv1[k] != tihv2[k])
-					collision = false;
-			if (collision)
-			{
-				for (unsigned t = 0; t < 16; ++t)
+				S1.m[1] = m1;
+				S1.computeQtp1(1);
+				if ( (S1.Qt(2)>>31) != (S1.Qt(0)>>31) )
+					continue;
+
+				S1.computeQtp1(2);
+				if ( (S1.Qt(3)>>31) != (S1.Qt(0)>>31) )
+					continue;
+
+				S1.computeQtp1(3);
+				if ( (S1.Qt(4)>>31) != (S1.Qt(0)>>31) )
+					continue;
+
+				S1.computeQtp1(4);
+				if ( (S1.Qt(5)>>31) != (S1.Qt(0)>>31) )
+					continue;
+
+				S1.computeQtp1(5);
+				if ( (S1.Qt(6)>>31) != (S1.Qt(0)>>31) )
+					continue;
+
+				S1.computeQtp1(6);
+				if ( (S1.Qt(7)>>31) != (S1.Qt(0)>>31) )
+					continue;
+					
+				int t = 7;
+				for (; t < 16; ++t)
 				{
-					std::cout << "block" << t << "=";
-					for (unsigned b = 0; b < 4; ++b)
-						std::cout << char((S1.m[t]>>(8*b))&0xFF);
-					std::cout << " ";
+					S1.computeQtp1(t);
+					if ( (S1.Qt(t+1)>>31) != (S1.Qt(0)>>31) )
+						break;
 				}
-				std::cout << std::endl;
-				std::cout << "\n ================================ \n FULL SOLUTION FOUND \n ================================" << std::endl;
-			        uint32 x = xrng128();
-			        std::string filename1 = "textcoll1_block2_" + boost::lexical_cast<string>(x) + ".txt";
-			        std::string filename2 = "textcoll2_block2_" + boost::lexical_cast<string>(x) + ".txt";
-			        ofstream of1(filename1.c_str(), ios::binary);
-			        ofstream of2(filename2.c_str(), ios::binary);
-			        if ((!of1) || (!of2)) {
-			                cerr << "Cannot open output files!" << endl;
-			        }
-			        save_block(of1, S1.m);
-			        save_block(of2, S1.m);
-			        of1.close();
-			        of2.close();
+				if (t < 16)
+					continue;
+
+				if (test_collision(ihv1, ihv2, S1.m))
+				{
+					for (unsigned t = 0; t < 16; ++t)
+					{
+						std::cout << "block" << t << "=";
+						for (unsigned b = 0; b < 4; ++b)
+							std::cout << char((S1.m[t]>>(8*b))&0xFF);
+						std::cout << " ";
+					}
+					std::cout << std::endl;
+					std::cout << "\n ================================ \n FULL SOLUTION FOUND \n ================================" << std::endl;
+				        uint32 x = xrng128();
+				        std::string filename1 = "textcoll1_block2_" + boost::lexical_cast<string>(x) + ".txt";
+				        std::string filename2 = "textcoll2_block2_" + boost::lexical_cast<string>(x) + ".txt";
+				        ofstream of1(filename1.c_str(), ios::binary);
+				        ofstream of2(filename2.c_str(), ios::binary);
+				        if ((!of1) || (!of2)) {
+				                cerr << "Cannot open output files!" << endl;
+					}
+				        save_block(of1, S1.m);
+				        save_block(of2, S1.m);
+				        of1.close();
+				        of2.close();
+				        exit(0);
+				}
+
 			}
 		}
-	}
-   });
+	});
+#else
+/*
+	MA = message_alphabet("()");
+	std::vector<uint32_t> words;
+	for (uint32_t m0 : MA.word_range(0))
+		words.emplace_back(m0);
+		
+	MA.set_byte_alphabet(0, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(1, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(2, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(3, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(4, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(5, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	MA.set_byte_alphabet(6, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	//MA.set_byte_alphabet(7, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+*/
+	run_workload(threads,
+	   [this]
+	   (size_t ji, size_t jn)
+	{
+		fullstate_t S1;
+		counter_exponential_print t15ok("t15ok");
+		counter_exponential_print t0ok("t0ok");
+		S1.Qt(-3) = ihv1[0]; S1.Qt(-2) = ihv1[3]; S1.Qt(-1) = ihv1[2]; S1.Qt(0) = ihv1[1];
+		while (true)
+		{
+		/*
+			S1.m[0] = MA.sampleword(0);
+			
+			if (S1.m[0] % 41 != ji) // split the search space
+				continue;
+				
+			S1.computeQtp1(0);
+			if ( (S1.Qt(1)>>31) != (S1.Qt(0)>>31) )
+				continue;
+			if (ji == 0) ++t0ok;
+
+		*/	
+			int t = 0;
+			for (; t < 15; ++t)
+			{
+				while (true) //for (uint32_t mt : words)
+				{
+					S1.m[t] = //mt; //words[xrng64() % words.size()];
+					MA.sampleword(t);
+					S1.computeQtp1(t);
+					if ( (S1.Qt(t+1)>>31) == (S1.Qt(t)>>31) )
+						break;
+				}
+/*				if ( (S1.Qt(t+1)>>31) != (S1.Qt(t)>>31) )
+				{
+					t -= 2; continue;
+					break;
+				}*/
+			}
+//			if (t < 12) continue;
+
+/*
+			for (uint32_t m1 : MA.word_range(1))
+			{
+				S1.m[1] = m1; S1.computeQtp1(1); if ( (S1.Qt(2)>>31) != (S1.Qt(0)>>31) ) continue;
+				
+			for (uint32_t m2 : words)//MA.word_range(13))
+			{
+				S1.m[2] = m2; S1.computeQtp1(2); if ( (S1.Qt(3)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m3 : words)//MA.word_range(13))
+			{
+				S1.m[3] = m3; S1.computeQtp1(3); if ( (S1.Qt(4)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m4 : words)//MA.word_range(13))
+			{
+				S1.m[4] = m4; S1.computeQtp1(4); if ( (S1.Qt(5)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m5 : words)//MA.word_range(13))
+			{
+				S1.m[5] = m5; S1.computeQtp1(5); if ( (S1.Qt(6)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m6 : words)//MA.word_range(13))
+			{
+				S1.m[6] = m6; S1.computeQtp1(6); if ( (S1.Qt(7)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m7 : words)//MA.word_range(13))
+			{
+				S1.m[7] = m7; S1.computeQtp1(7); if ( (S1.Qt(8)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m8 : words)//MA.word_range(13))
+			{
+				S1.m[8] = m8; S1.computeQtp1(8); if ( (S1.Qt(9)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m9 : words)//MA.word_range(13))
+			{
+				S1.m[9] = m9; S1.computeQtp1(9); if ( (S1.Qt(10)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m10 : words)//MA.word_range(13))
+			{
+				S1.m[10] = m10; S1.computeQtp1(10); if ( (S1.Qt(11)>>31) != (S1.Qt(0)>>31) ) continue;
+
+			for (uint32_t m11 : words)//MA.word_range(13))
+			{
+				S1.m[11] = m11; S1.computeQtp1(11); if ( (S1.Qt(12)>>31) != (S1.Qt(0)>>31) ) continue;
+
+			for (uint32_t m12 : words)//MA.word_range(13))
+			{
+				S1.m[12] = m12; S1.computeQtp1(12); if ( (S1.Qt(13)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m13 : words)//MA.word_range(13))
+			{
+				S1.m[13] = m13; S1.computeQtp1(13); if ( (S1.Qt(14)>>31) != (S1.Qt(0)>>31) ) continue;
+			for (uint32_t m14 : words)//MA.word_range(14))
+			{
+				S1.m[14] = m14; S1.computeQtp1(14); if ( (S1.Qt(15)>>31) != (S1.Qt(0)>>31) ) continue;
+*/
+			for (uint32_t m15 : MA.word_range(15))
+			{
+				S1.m[15] = m15; //S1.computeQtp1(15); if ( (S1.Qt(16)>>31) != (S1.Qt(0)>>31) ) continue;
+				if (ji == 0) ++t15ok;
+				if (test_collision(ihv1, ihv2, S1.m))
+				{
+					for (unsigned t = 0; t < 16; ++t)
+					{
+						std::cout << "block" << t << "=";
+						for (unsigned b = 0; b < 4; ++b)
+							std::cout << char((S1.m[t]>>(8*b))&0xFF);
+						std::cout << " ";
+					}
+					std::cout << std::endl;
+					std::cout << "\n ================================ \n FULL SOLUTION FOUND \n ================================" << std::endl;
+				        uint32 x = xrng128();
+				        std::string filename1 = "textcoll1_block2_" + boost::lexical_cast<string>(x) + ".txt";
+				        std::string filename2 = "textcoll2_block2_" + boost::lexical_cast<string>(x) + ".txt";
+				        ofstream of1(filename1.c_str(), ios::binary);
+				        ofstream of2(filename2.c_str(), ios::binary);
+				        if ((!of1) || (!of2)) {
+				                cerr << "Cannot open output files!" << endl;
+					}
+				        save_block(of1, S1.m);
+				        save_block(of2, S1.m);
+				        of1.close();
+				        of2.close();
+				        exit(0);
+				}
+			} // m15
+/*			} // m14
+			} // m13
+			} // m12
+			} // m11
+			} // m10
+			} // m9
+			} // m8
+			} // m7
+			} // m6
+			} // m5
+			} // m4
+			} // m3
+			} // m2
+			} // m1*/
+		}
+	});
+#endif
 }
 
 int textcoll(parameters_type& parameters)
